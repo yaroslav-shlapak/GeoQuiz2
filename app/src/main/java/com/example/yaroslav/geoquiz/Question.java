43415a280368adf8aeb03e8c8d307eb4e1106b13
Yaroslav Shlapak <yaroslav.shlapak@gmail.com>
2016-05-20 00:05:48 +0300

    Page 71

---
+++ app/src/main/java/com/example/yaroslav/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.example.yaroslav.geoquiz;
+
+/**
+ * Created by yaroslav on 19.05.16.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+
+}
--- app/src/main/java/com/example/yaroslav/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/yaroslav/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
@@ -11,9 +12,24 @@ import butterknife.ButterKnife;
 
 public class QuizActivity extends AppCompatActivity {
 
-    @Bind(R.id.true_button) Button mTrueButton;
-    @Bind(R.id.false_button) Button mFalseButton;
+    @Bind(R.id.true_button)
+    Button mTrueButton;
+    @Bind(R.id.false_button)
+    Button mFalseButton;
+    @Bind(R.id.text_view)
+    TextView mTextView;
+    @Bind(R.id.next_button)
+    Button mNextButton;
 
+    Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +38,46 @@ public class QuizActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        updateQuestion();
+
+
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
-            }
+                checkAnswer(false);            }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getmTextResId();
+        mTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.id.true_button;
+        } else {
+            messageResId = R.id.false_button;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
